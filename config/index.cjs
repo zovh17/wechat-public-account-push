aa33caef1a354ae866c1c02651b5d36180cd6b43
zovh17 <119491621+zovh17@users.noreply.github.com>
2023-11-08 19:25:34 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'rBwtx1Pqqyut0CUAJty74akd68UUc8uviFeCl-VQqQQ',
 
   CALLBACK_USERS: [
     {
